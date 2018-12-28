ccaecbfec3248330fc04791d38248fa5d73258ba
omprakash1989 <oppradhan2011@gmail.com>
2018-12-29 01:38:15 +0530

    Added basis model and migrations and db interactions.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, redirect, url_for, request, render_template, make_response, session, abort, flash
 from werkzeug import secure_filename
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import sqlite3
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.secret_key='Raj'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+
+db = SQLAlchemy()
+db.init_app(app)
+Migrate(app, db)
+
 
 @app.route('/')
 def home():
@@ -17,9 +23,28 @@ def home():
 def index():
     return render_template('Admin/login.html')
 
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
+
+        # Add an user
+        user = User(email="om@gmail.com", password='abcd')
+        db.session.add(user)
+        db.session.commit()
+        print("Added user: ")
+
+        # Get all user
+        users = db.session.query(User).all()
+        for user in users:
+            print(user.email)
+            print(user.password)
+            print("==============")
+
+        # Filter on the basis of email.
+        user = db.session.query(User).filter_by(email='om@gmail.com').first()
+        print("Filtered on the basis of user email, and password is: {}".format(user.password))
+
         return redirect(url_for('dashboard'))
     return render_template('Admin/login.html')
 
@@ -65,3 +90,14 @@ def edit_post():
 @app.route('/logout')
 def logout():
     return redirect(url_for('login'))
+
+
+class User(db.Model):
+    """
+    Model to save the User.
+    """
+    __tablename__ = 'user'
+
+    id = db.Column('id', db.Integer, nullable=False, primary_key=True)
+    email = db.Column('email', db.String, nullable=False)
+    password = db.Column('password', db.String, nullable=False)
